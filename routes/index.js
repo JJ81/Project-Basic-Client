bf1dee3729847a213b5c68fe9e93620a96d9191d
Yun <cheese10yun@gmail.com>
2017-02-02 12:35:34 +0900

    어드민 로그인 작업

--- routes/index.js
+++ routes/index.js
@@ -14,148 +14,108 @@ require('../helpers/helpers');
 
 
 passport.serializeUser((user, done) => {
-	console.log(user);
-	done(null, user);
+    console.log(user);
+    done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-	console.log(user);
-	done(null, user);
+    console.log(user);
+    done(null, user);
 });
 
 var isAuthenticated = (req, res, next) => {
-	if (req.isAuthenticated())
-		return next();
-	res.redirect('/login');
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/login');
 };
 
 passport.use(new LocalStrategy({
-	usernameField: 'agent',
-	passwordField: 'password',
-	passReqToCallback: true
-}, (req, agent, password, done) => {
-	connection.query(QUERY.AGENT.login, [agent], (err, data) => {
-		if (err) {
-			return done(null, false);
-		} else {
-			if (data.length === 1) {
-				if (!bcrypt.compareSync(password, data[0].password)) {
-					console.log('password is not matched.');
-					return done(null, false);
-				} else {
-					console.log('password is matched.');
-					console.info(data[0]);
-					return done(null, {
-						'agent': data[0].code,
-						'layer': data[0].layer,
-						'parent_id': data[0].parent_id,
-						'top_parent_id': data[0].top_parent_id,
-						'balance': data[0].balance
-					});
-				}
-			} else {
-				return done(null, false);
-			}
-		}
-	});
-}
+        usernameField: 'agent',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, (req, agent, password, done) => {
+        connection.query(QUERY.AGENT.login, [agent], (err, data) => {
+            if (err) {
+                return done(null, false);
+            } else {
+                if (data.length === 1) {
+                    if (!bcrypt.compareSync(password, data[0].password)) {
+                        console.log('password is not matched.');
+                        return done(null, false);
+                    } else {
+                        console.log('password is matched.');
+                        console.info(data[0]);
+                        return done(null, {
+                            'agent': data[0].code,
+                            'layer': data[0].layer,
+                            'parent_id': data[0].parent_id,
+                            'top_parent_id': data[0].top_parent_id,
+                            'balance': data[0].balance
+                        });
+                    }
+                } else {
+                    return done(null, false);
+                }
+            }
+        });
+    }
 ));
 
 router.get('/login', function (req, res) {
-	if (req.user == null) {
-		res.render('login', {
-			current_path: 'login',
-			title: PROJ_TITLE + 'login'
-		});
-	} else {
-		res.redirect('/');
-	}
+    if (req.user == null) {
+        res.render('login', {
+            current_path: 'login',
+            title: PROJ_TITLE + 'login'
+        });
+    } else {
+        res.redirect('/');
+    }
 });
 
 router.post('/login', passport.authenticate('local', {
-	failureRedirect: '/login',
-	failureFlash: true
+    failureRedirect: '/login',
+    failureFlash: true
 }), function (req, res) {
-	res.redirect('/');
+    res.redirect('/');
 });
 
 router.get('/logout', isAuthenticated, (req, res) => {
-	req.logout();
-	res.redirect('/');
+    req.logout();
+    res.redirect('/');
 });
 
 
-router.get('/', (req, res) => {
-    
-	console.log('=======================================');
-    
+router.get('/', isAuthenticated, (req, res) => {
     res.render('index', {
         current_path: 'INDEX',
         title: PROJ_TITLE,
         loggedIn: req.user,
     });
-    
-	// async.parallel(
-	// 	[
-	// 		(cb) => {
-	// 			connection.query(QUERY.HOME.GetRecomList, (err, rows) => {
-	// 				if (!err) {
-	// 					cb(null, rows);
-	// 				} else {
-	// 					console.error(err);
-	// 					cb(err, null);
-	// 				}
-	// 			});
-	// 		},
-	// 		(cb) => {
-	// 			connection.query(QUERY.HOME.GetNavAllList, (err, rows) => {
-	// 				if (!err) {
-	// 					cb(null, rows);
-	// 				} else {
-	// 					console.error(err);
-	// 					cb(err, null);
-	// 				}
-	// 			});
-	// 		}
-	// 	], (err, result) => {
-	// 	if (!err) {
-	// 		res.render('index', {
-	// 			current_path: 'INDEX',
-	// 			title: PROJ_TITLE,
-	// 			loggedIn: req.user,
-	// 			recomList: result[0],
-	// 			contentlist: result[1]
-	// 		});
-	// 	} else {
-	// 		console.error(err);
-	// 		throw new Error(err);
-	// 	}
-	// });
 });
 
 router.get('/test', (req, res) => {
-	res.json({result: 'Hello World'});
+    res.json({result: 'Hello World'});
 });
 
 
 // TEST CSRF token
 const csrf = require('csurf');
-const csrfProtection = csrf({ cookie: true });
+const csrfProtection = csrf({cookie: true});
 const bodyParser = require('body-parser');
-const parseForm = bodyParser.urlencoded({extended:false});
+const parseForm = bodyParser.urlencoded({extended: false});
 // todo app.js에서 사용하고 있는 global에 바인딩된 것은 왜 사용하지 못하지?
 
 
 router.get('/test/form', csrfProtection, (req, res) => {
-	res.render('form', {
-		title : PROJ_TITLE
-		,csrfToken : req.csrfToken()
-		// test : mysql_location // this is working!!
-	});
+    res.render('form', {
+        title: PROJ_TITLE
+        , csrfToken: req.csrfToken()
+        // test : mysql_location // this is working!!
+    });
 });
 
 router.post('/test/form/submit', parseForm, csrfProtection, (req, res) => {
-	res.send('data is being processed');
+    res.send('data is being processed');
 });
 
 module.exports = router;
